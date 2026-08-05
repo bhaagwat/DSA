30fc383baf1bfe115622fa8aac21708b50da5ae0
Bhaagwat <bhaagwatvikram@gmail.com>
2026-08-05 22:43:05 +0530

    Time: 21 ms (64.38%) | Memory: 17.6 MB (23.78%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,47 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> ans;
+    vector<vector<int>> fourSum(vector<int>& nums, int tar) {
         int n = nums.size();
+        vector<vector<int>> ans;   // Missing
 
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
 
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1])
-                    continue;
+            for(int j = i + 1; j < n; ){
+                int p = j + 1, q = n - 1;
 
-                int p = j + 1;
-                int q = n - 1;
+                while(p < q){
+                    long long sum = (long long)nums[i] + (long long)nums[j]
+                                  + (long long)nums[p] + (long long)nums[q];
 
-                while (p < q) {
-                    long long sum = (long long)nums[i] +
-                                    (long long)nums[j] +
-                                    (long long)nums[p] +
-                                    (long long)nums[q];
-
-                    if (sum < (long long)target) {
+                    if(sum < (long long)tar){
                         p++;
                     }
-                    else if (sum > (long long)target) {
+                    else if(sum > (long long)tar){
                         q--;
                     }
-                    else {
+                    else{
                         ans.push_back({nums[i], nums[j], nums[p], nums[q]});
-
                         p++;
                         q--;
 
-                        while (p < q && nums[p] == nums[p - 1])
-                            p++;
-
-                        while (p < q && nums[q] == nums[q + 1])
-                            q--;
+                        while(p < q && nums[p] == nums[p-1]) p++;
+                        
                     }
                 }
+
+                j++;
+
+                while(j < n && nums[j] == nums[j-1]) j++;
             }
         }
 
